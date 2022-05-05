30113a07d1c9703f9cbdc26fcd61dd645e05deaa
dyonatamachado <dyonatamachado95@gmail.com>
2022-05-05 19:51:33 -0300

    Inicialização dos Projetos

---
+++ backend/FinanceGO.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FinanceGO.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ backend/FinanceGO.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FinanceGO.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
