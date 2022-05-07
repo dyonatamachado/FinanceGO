4c791337bd81d666b74ec2dd996bb901b9318a51
dyonatamachado <dyonatamachado95@gmail.com>
2022-05-07 15:49:03 -0300

    Implementado DespesaController

--- backend/FinanceGO.API/Controllers/DespesaController.cs
+++ backend/FinanceGO.API/Controllers/DespesaController.cs
@@ -1,6 +1,10 @@
 using System.Threading.Tasks;
 using FinanceGO.Application.Commands.DespesaCommands.CreateDespesa;
+using FinanceGO.Application.Commands.DespesaCommands.DeleteDespesa;
+using FinanceGO.Application.Commands.DespesaCommands.UpdateDespesa;
 using FinanceGO.Application.Queries.DespesaQueries.GetDespesaById;
+using FinanceGO.Application.Queries.DespesaQueries.GetDespesas;
+using FinanceGO.Application.Queries.DespesaQueries.GetDespesasByMonth;
 using FinanceGO.Application.ViewModels;
 using FinanceGO.Core.Results;
 using MediatR;
@@ -19,6 +23,28 @@ namespace FinanceGO.API.Controllers
             _mediator = mediator;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetDespesas(GetDespesasQuery query)
+        {
+            var despesas = await _mediator.Send(query);
+
+            if(despesas.Count == 0) return NoContent();
+
+            return Ok(despesas);
+        }
+
+        [HttpGet("{mes}/{ano}")]
+        public async Task<IActionResult> GetDespesasByMonth(int mes, int ano)
+        {
+            var query = new GetDespesasByMonthQuery(mes, ano);
+
+            var despesas = await _mediator.Send(query);
+
+            if(despesas.Count == 0) return NoContent();
+
+            return Ok(despesas);
+        }
+
         [HttpGet("{id}")]
         public async Task<IActionResult> GetDespesaById(int id)
         {
@@ -31,8 +57,6 @@ namespace FinanceGO.API.Controllers
                 
             return Ok(despesa);
         }
-        
-
 
         [HttpPost]
         public async Task<IActionResult> CreateDespesa(CreateDespesaCommand command)
@@ -50,6 +74,34 @@ namespace FinanceGO.API.Controllers
                 return BadRequest();
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDespesa(int id, [FromBody] UpdateDespesaCommand command)
+        {
+            var resultado = await _mediator.Send(command);
 
+            if(resultado is RegistroNaoEncontradoResult) 
+                return NotFound();
+            else if(resultado is RegistroDuplicadoResult) 
+                return BadRequest("Já existe despesa com a mesma descrição cadastrada neste mês");
+            else if(resultado is RegistroAtualizadoComSucesso) 
+                return NoContent();
+            else 
+                return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDespesa(int id)
+        {
+            var query = new DeleteDespesaCommand(id);
+
+            var resultado = await _mediator.Send(query);
+
+            if(resultado is RegistroNaoEncontradoResult)
+                return NotFound();
+            else if(resultado is DeletadoComSucessoResult)
+                return NoContent();
+            else
+                return BadRequest();            
+        }
     }
 }
